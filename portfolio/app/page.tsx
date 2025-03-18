e6143b5e9ee4dd3e42b385b0120409d1c7cdb9cc
Glago Gideon Elorm <glagogideonelorm2006@gmail.com>
2025-03-18 14:05:57 +0000

    Initialize project with package.json and Swiper module styles

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1>Welcome to My Portfolio</h1>
+      
+      <p>This is the home page content.</p>
+    </main>
+  );
+}
